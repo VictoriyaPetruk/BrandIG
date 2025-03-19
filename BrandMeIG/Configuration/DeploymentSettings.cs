8e9bbd6ce22f4df7002c4f50896193bd2d67c1df
Viktoriia <69762201+VictoriyaPetruk@users.noreply.github.com>
2025-03-19 13:03:44 +0100

    add settings

---
+++ BrandMeIG/Configuration/DeploymentSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace BrandMeIG.Configuration
+{
+    public class DeploymentSettings
+    {
+        public string InstagramToken { get; set; }
+        public string BusinessId { get; set; }
+        public string GptToken { get; set; }
+
+        public Uri InstagramBaseAPIUri { get; set; }
+        //public string FaceBookAppSecret { get; set; }
+        //public string FaceBookAppId { get; set; }
+        //public string BusinessAccount { get; set; }
+        //public string PageID { get; set; }
+    }
+}
