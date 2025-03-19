1985611f5d032ea0b7ccd29b21cfcc2e68ca073a
Viktoriia <69762201+VictoriyaPetruk@users.noreply.github.com>
2025-03-19 13:03:53 +0100

    Create InstagramSettings.cs

---
+++ BrandMeIG/Configuration/InstagramSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BrandMeIG.Configuration
+{
+    public class InstagramSettings
+    {
+        public string IGToken { get; set; }
+        public string BusinessId { get; set; }
+    }
+}
